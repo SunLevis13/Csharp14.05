9c8b250c230abfb34ef8d5c650dfb21856fe0486
Daria <dashazoueva@gmail.com>
2022-05-16 00:30:44 +0300

    exercise4

---
+++ exercise4/Program.cs
@@ -0,0 +1,13 @@
+﻿void Number(int a)
+{
+    if (a % 7 == 0 & a % 13 == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        Console.WriteLine("Не кратно");
+    }
+}
+
+Number(14);
