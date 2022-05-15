58722652fe814b2d4539f10a12ef7a30723c8ea4
Daria <dashazoueva@gmail.com>
2022-05-15 19:50:58 +0300

    exercise2

---
+++ exercise2/Program.cs
@@ -0,0 +1,15 @@
+﻿int MaxNumber (int number)
+{
+    int number1 = number % 10;
+    int number2 = number / 10;
+    if(number2 > number1){
+        return number2;
+    }
+    else
+    {
+        return number1;
+    }
+}
+int RandomNumber = new Random().Next(10,100);
+Console.WriteLine (MaxNumber(RandomNumber));
+Console.WriteLine(RandomNumber);
