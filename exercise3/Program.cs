29fbab59045276840c9f4a9b4b22284010998741
Daria <dashazoueva@gmail.com>
2022-05-15 20:30:43 +0300

    exercise3

---
+++ exercise3/Program.cs
@@ -0,0 +1,15 @@
+﻿void F(int a, int b)
+{
+    if (a % b == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        Console.WriteLine("Не кратно");
+    }
+}
+
+int a = 34;
+int b = 2;
+F(a, b);
