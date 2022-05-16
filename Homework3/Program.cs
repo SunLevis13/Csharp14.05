e1006f542870a996648c2a42acfa98cb2d655345
Daria <dashazoueva@gmail.com>
2022-05-16 13:50:07 +0300

    correcthw3

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -1,12 +1,20 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-void Numb (int a)
-{if(a > 99)
- {
-    Console.WriteLine($"{a % 10}");
- }
- else
- {
-     Console.WriteLine("no third numb");
- }
+void Numb(int a)
+{
+    int thirdnumb = 1;
+    string txt;
+
+    while (a > 99)
+    {
+        thirdnumb = a % 10;
+        a = a / 10;
+
+    }
+    txt = $"{thirdnumb}";
+    if (thirdnumb == 1)
+    {
+        txt = "no third numb";
+    }
+    Console.WriteLine(txt);
 }
-Numb(178);
+Numb(1789);
